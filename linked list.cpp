d8e598f9b86b4394bb2cc87883df88a81ccec59c
Restyled.io <commits@restyled.io>
2023-08-15 12:56:03 +0000

    Restyled by clang-format

--- linked list.cpp
+++ linked list.cpp
@@ -1,43 +1,39 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 class node {
 public:
-    int data;
-    node *next;
-    node() {
-        data=0;
-        next= NULL;
-    }
+  int data;
+  node *next;
+  node() {
+    data = 0;
+    next = NULL;
+  }
 };
 
-
 int main() {
-    node *head;
-    node *one= NULL;
-    node *two= NULL;
-    node *three= NULL;
+  node *head;
+  node *one = NULL;
+  node *two = NULL;
+  node *three = NULL;
 
+  one = new node();
+  two = new node();
+  three = new node();
 
-    one = new node();
-    two = new node();
-    three = new node();
+  one->data = 1;
+  two->data = 2;
+  three->data = 3;
 
+  head = one;
+  one->next = two;
+  two->next = three;
+  three->next = NULL;
+  node *temp = head;
 
-    one->data = 1;
-    two->data = 2;
-    three->data = 3;
-
-    head = one;
-    one->next = two;
-    two->next = three;
-    three->next = NULL;
-    node* temp= head;
-
-    while(temp != NULL) {
-        cout<<temp->data<<" ";
-        temp = temp->next;
-    }
-    return 0;
-
+  while (temp != NULL) {
+    cout << temp->data << " ";
+    temp = temp->next;
+  }
+  return 0;
 }
